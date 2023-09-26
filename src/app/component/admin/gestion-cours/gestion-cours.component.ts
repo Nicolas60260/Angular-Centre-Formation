4f7f335b2907afb15fcf93d5b28b3d621ec3b98c
hgayssant <gayssant.harold@gmail.com>
2023-09-26 11:33:37 +0200

    26/09/2023 Ajout coursAdmin et autres fonctionalités HAROLD 11:33

--- src/app/component/admin/gestion-cours/gestion-cours.component.ts
+++ src/app/component/admin/gestion-cours/gestion-cours.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cours } from 'src/app/models/cours';
+import { CoursService } from 'src/app/service/site/cours.service';
 
 @Component({
   selector: 'app-gestion-cours',
@@ -6,11 +8,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gestion-cours.component.css']
 })
 export class GestionCoursComponent implements OnInit {
+listeCours!:Cours[];
+cours!:Cours;
+selectedFile!: File;
 
-  
+constructor(private coursService:CoursService){}
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  this.afficherAll();
+  this.cours=new Cours();
   }
 
+  onFileSelected(event:any){
+    this.selectedFile = event.target.files[0];
+  }
 
+  afficherAll(){
+    this.coursService.getAll().subscribe(
+      response=>{this.listeCours=response,
+      this.listeCours.forEach(cours => {
+        cours.urlfichier = "http://localhost:8018/dossiercours/" + cours.fichier
+      })},
+      error=>{console.error("Impossible d'afficher la liste des cours");
+      }
+    )
+  }
+
+  enregistrerCours(){
+    let formdata = new FormData();
+    if (this.selectedFile) {
+      formdata.append("fichier",this.selectedFile)
+    }
+    if (this.cours.id) {
+      formdata.append('id', this.cours.id.toString());
+    }else{
+      formdata.append('id', "0");
+
+    }
+    if(this.cours.duree) {
+      formdata.append('duree', this.cours.duree.toString());
+    }else{
+      formdata.append('duree', "0");
+
+    }
+    formdata.append('nom', this.cours.nom);
+
+    this.coursService.addCours(formdata).subscribe(
+      response=>{console.log("Cours enregistré"), this.afficherAll(), this.cours= new Cours()},
+      error=>{console.error("Impossible d'enregistrer le cours");
+      }
+    )
+  }
+
+  supprimerCours(id:number){
+    this.coursService.deleteCours(id).subscribe(
+      response=>{console.log("Cours supprimé"),this.afficherAll()},
+      error=>{console.error("Impossible de supprimer le cours");
+      }
+    )
+  }
+
+  modifierCours(cours:Cours){
+    this.cours=cours;
+    this.afficherAll();
+  }
 }
--- src/app/component/admin/gestion-prospect/gestion-prospect.component.ts
+++ src/app/component/admin/gestion-prospect/gestion-prospect.component.ts
@@ -11,12 +11,70 @@ import { ProspectService } from 'src/app/service/site/prospect.service';
 export class GestionProspectComponent implements OnInit {
   listeProspect !: Prospect[];
   prospect !: Prospect;
+  csvfile!: File;
+  fileContent: string | undefined;
 
-  constructor(private prospectService: ProspectService,private route:Router) { }
+  constructor(private prospectService: ProspectService, private route: Router) { }
 
   ngOnInit(): void {
     this.afficherAll(),
-      this.prospect=new Prospect()
+      this.prospect = new Prospect()
+  }
+
+  onFileSelected(event: any) {
+    this.csvfile = event.target.files[0];
+  }
+
+
+  validerCsv() {
+    console.log(this.csvfile)
+    if (this.csvfile) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        if (e.target) {
+          this.fileContent = e.target.result as string;
+          console.log("Contenu du fichier :", this.fileContent);
+          const prospectsList = this.parseCSVContent(this.fileContent);
+          console.log(prospectsList);
+          prospectsList.forEach(prospect=>{
+            this.prospectService.addProspect(prospect).subscribe(
+              response => {
+                this.prospect = new Prospect(); this.afficherAll();
+                console.log("Prospect enregistré")
+              },
+              error => { console.error("Impossible d'enregistrer le prospect") }
+            )
+          })
+        }
+      };
+
+      reader.readAsText(this.csvfile);
+    }
+  }
+
+  parseCSVContent(content: string) {
+    const lines = content.split('\n');
+    const prospects = [];
+
+    // Parcourez chaque ligne du fichier CSV
+    for (let i = 1; i < lines.length; i++) { // Commencez à 1 pour ignorer l'en-tête
+      const line = lines[i].trim();
+      if (line) {
+        const values = line.split(',');
+
+        // Vérifiez que la ligne a le bon nombre de valeurs
+        if (values.length === 5) {
+          const [nom, prenom, mail, telephone, statut] = values;
+          const prospect = new Prospect(undefined, nom, prenom, mail, telephone, statut);
+          prospects.push(prospect);
+        } else {
+          console.warn(`La ligne ${i + 1} du fichier CSV n'a pas le bon nombre de valeurs.`);
+        }
+      }
+    }
+
+    return prospects;
   }
 
   afficherAll() {
@@ -32,34 +90,37 @@ export class GestionProspectComponent implements OnInit {
     )
   }
 
-  enregistrerProspect(){
+  enregistrerProspect() {
     this.prospectService.addProspect(this.prospect).subscribe(
-      response=>{this.prospect=new Prospect(); this.afficherAll();
-      console.log("Prospect enregistré")},
-      error => {console.error("Impossible d'enregistrer le prospect")}
+      response => {
+        this.prospect = new Prospect(); this.afficherAll();
+        console.log("Prospect enregistré")
+      },
+      error => { console.error("Impossible d'enregistrer le prospect") }
     )
   }
 
-  modifierProsepct(prospect:Prospect){
-    this.prospect=prospect;
+  modifierProsepct(prospect: Prospect) {
+    this.prospect = prospect;
     this.afficherAll()
   }
 
-  afficherCommentaire(id:number){
+  afficherCommentaire(id: number) {
     this.route.navigateByUrl(`adminCommentaire/${id}`)
   }
-  afficherAppel(id:number){
+  afficherAppel(id: number) {
     this.route.navigateByUrl(`adminAppel/${id}`)
   }
 
-  inscrireParticipant(prospect:Prospect){
+  inscrireParticipant(prospect: Prospect) {
     console.log(prospect)
     this.prospectService.inscrireParticipant(prospect).subscribe(
-      response=>{console.log("Prospect #" + prospect.id + " inscrit")},
-      error => {console.error("Impossible d'ajouter le prospect dans les inscrits");
+      response => { console.log("Prospect #" + prospect.id + " inscrit") },
+      error => {
+        console.error("Impossible d'ajouter le prospect dans les inscrits");
       }
     );
 
-  
+
   }
 }
