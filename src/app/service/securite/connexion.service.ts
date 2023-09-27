cd10e80815346dc7adfe77d264196acba27ed064
hgayssant <gayssant.harold@gmail.com>
2023-09-27 15:15:36 +0200

    Ajout de la gestion des roles 27/09/2023 15:15

--- src/app/service/securite/connexion.service.ts
+++ src/app/service/securite/connexion.service.ts
@@ -14,6 +14,8 @@ export class ConnexionService {
   CatchToken(u:Utilisateur){
 
     return this.http.post<AuthentificationResponse>("http://localhost:8018/login",u);
-
+  }
+  getUser(username: string) {
+    return this.http.get<Utilisateur>(`http://localhost:8018/utilisateur/public/${username}`)
   }
 }
